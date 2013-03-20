9c77ba604539eedf99718076d8c01c66b0ed56d5
yfaming <yfaming@gmail.com>
2013-03-21 01:25:21 +0800

    改掉一个让我纠结了一晚上的 bug。
    SHIT！

--- FHN.py
+++ FHN.py
@@ -22,11 +22,11 @@ def teardown_request(exception):
 def show_entries():
 	db = g.conn.fhn
 	#按 points 倒排，以表示受欢迎程度
-	entries = db.urls.find().sort('points', DESCENDING).limit(32)
+	entries = list(db.urls.find().sort('points', DESCENDING).limit(32))
 	return render_template('index.html', entries=enumerate(entries, start=1))
 
 @app.route('/newest')
-def show_entries():
+def show_newest():
 	db = g.conn.fhn
 	#这个查询得修改完善
 	entries = [ e for e in db.urls.find().limit(32)]
